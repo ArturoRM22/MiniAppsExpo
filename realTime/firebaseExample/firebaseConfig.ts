da574f450a6cff46276f443f522c5fd5ec0c1754
ArturoRM22 <A01643269@tec.mx>
2024-11-10 22:14:17 -0600

    ggs

--- realTime/firebaseExample/firebaseConfig.ts
+++ realTime/firebaseExample/firebaseConfig.ts
@@ -8,7 +8,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
 
 };
-
+         
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); 
